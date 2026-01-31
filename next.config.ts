097fc5359d4d520be3a5f6ca6b8ee0b4d8f6c4b0
LennyKiang <kenny712126@gmail.com>
2026-01-31 17:34:08 -0500

    setting up next

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  basePath: "/kennyis.me",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
